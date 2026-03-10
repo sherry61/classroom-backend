4e21564b2324f84fd7f08492a75e245f8002a6f5
sherry61 <3154368796@qq.com>
2026-03-10 20:20:29 +0800

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import subjectsRouter from "./routes/subjects";
 
 const app = express();
 const PORT = 8000;
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
+
 app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
